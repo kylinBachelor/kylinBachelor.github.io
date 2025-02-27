92e2c840445f43a9307cdc095d019a732cbbc5e9
congpeitong <congpeitong@shundesoft.com>
2025-02-27 09:41:18 +0800

    svg

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -20,7 +20,7 @@ export default defineThemeConfig({
   locales: {
     '/': {
       profile: {
-        avatar: 'https://theme-plume.vuejs.press/plume.png',
+        avatar: 'https://kylinBachelor.github.io/plume.png',
         name: '攻城狮',
         description: '脱去皮囊，无非二百零六骨，穿上衣裳，可有一万八千相，观美人如白骨，使我无欲，观白骨如美人，使我无惧，君子当守正念，以养浩然之气，斩断色欲，利于不败，色而不淫，方为丈夫',
         circle: true,
